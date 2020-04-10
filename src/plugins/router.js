50e5561c55f4b84b756ab35b9de47d1d38e45664
glvchv <glavchev.vld@gmail.com>
2020-04-10 16:01:32 +0300

    lazy loaded

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,20 +3,20 @@ import VueRouter from 'vue-router';
 import Home from '@/components/Home';
 import Login from '@/components/user/Login';
 import Register from '@/components/user/Register';
-import Create from '@/components/Create';
+//import Create from '@/components/Create';
 import NotFound from '@/components/NotFound';
 import EventDetails from '@/components/EventDetails';
-import MyEvents from '@/components/user/MyEvents';
+//import MyEvents from '@/components/user/MyEvents';
 import firebase from 'firebase';
 
 const routes = [
-    {path: '/', component: Home},
-    {path: '/login', component: Login, meta: {requiresGuest: true}},
-    {path: '/register', component: Register, meta: {requiresGuest: true}},
-    {path: '/create', component: Create, meta: {requiresAuth: true}},
-    {path: '/details/:id', name: 'details', component: EventDetails, meta: {requiresAuth: true}},
-    {path: '/myevents', component: MyEvents, meta: {requiresAuth: true}},
-    {path: '*', component: NotFound}
+    { path: '/', component: Home },
+    { path: '/login', component: Login, meta: { requiresGuest: true } },
+    { path: '/register', component: Register, meta: { requiresGuest: true } },
+    { path: '/create', component: () => import('@/components/Create'), meta: { requiresAuth: true } },
+    { path: '/details/:id', name: 'details', component: EventDetails, meta: { requiresAuth: true } },
+    { path: '/myevents', component: () => import('@/components/user/MyEvents'), meta: { requiresAuth: true } },
+    { path: '*', component: NotFound }
 ]
 
 Vue.use(VueRouter);
@@ -29,12 +29,12 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
     const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
     if (requiresAuth) {
-         firebase.auth().onAuthStateChanged(u => u ? next() : next('/login'));
+        firebase.auth().onAuthStateChanged(u => u ? next() : next('/login'));
     } else {
         next();
     }
     const requiresGuest = to.matched.some(record => record.meta.requiresGuest);
-    if(requiresGuest) {
+    if (requiresGuest) {
         firebase.auth().onAuthStateChanged(u => u ? next('/') : next());
     } else {
         next();
